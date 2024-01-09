e3beb4b02c6fbfbd1270b4ac3bdca6012eaeec49
MohanaPoojary <mohan.poojary@manipal.net>
2024-01-09 15:56:39 +0530

    commit all

--- e-cart/src/pages/shop/shop.jsx
+++ e-cart/src/pages/shop/shop.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import { PRODUCT } from '../../product'
+import { Products } from './products'
+import './shop.css';
 export const Shop = () => {
   return (
-    <div>shoping</div>
+    <div className="shop">
+      <div className="shoptitle">
+        <h1> Big Shopping</h1>
+      </div>
+      <div className="products">
+        {PRODUCT.map((product)=>(
+          <Products data={product}/>
+        ))}
+      </div>
+
+    </div>
   )
 }
---
+++ e-cart/src/product.js
@@ -0,0 +1,24 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.jpeg';
+import product3 from './assets/3.webp';
+
+export const PRODUCT =[
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Camera",
+        price: 999.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Laptop",
+        price: 999.0,
+        productImage: product3,
+    },
+];
